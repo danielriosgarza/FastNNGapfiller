1105fba11259daeb0fba1ff634b87c437590e868
Biomerene <hijisktr@gmail.com>
2022-01-20 17:14:30 +0100

    updated comments

--- scripts/NN_Predictor.py
+++ scripts/NN_Predictor.py
@@ -15,13 +15,16 @@ path = Path.cwd()
 sys.path.append(path)
 
 
+#Function that loads the Neural network, maybe unneccesary; path is path to .h5 file
 def load_NN(path):
     return tf.keras.models.load_model(path, custom_objects={"custom_loss": 'binary_crossentropy'})
 
-
+#Function that makes a prediction based on input_data using Neural Network (NN)
 def make_prediction(input_data, NN):
+
     single_input=False
     #test for single input (trips up NN)
+    #workaround by just making the same prediction twice
     if np.ndim(input_data) == 1:
         single_input=True
         input_data = np.tile(input_data,[2,1])
@@ -33,7 +36,7 @@ def make_prediction(input_data, NN):
     t_result = network.predict(input_data)
     prediction = np.asarray(t_result)
 
-    #ugly i know. but do not have a solution at this point
+    #ugly i know. but do not have a different solution at this point
     if single_input:
         prediction  = prediction[0].T
     else:
@@ -41,7 +44,7 @@ def make_prediction(input_data, NN):
 
     return prediction
 
-
+#function that generates a binary input based on a list of reaction ids
 def convert_reaction_list(reaction_set):
     #load reaction ids
     file_path = os.path.join(path.parent, 'files', 'NN')
