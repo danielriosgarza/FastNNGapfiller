69b6ec2c6fce7b6776eadd143c1a26f36e91c3a9
Daniel Garza <danielriosgarza@gmail.com>
2023-03-05 08:38:11 +0100

    fixed model build script

--- scripts/build_model.py
+++ scripts/build_model.py
@@ -25,11 +25,31 @@ reactions_aliases_dict = reactions_helper.loadMSAliases()
 reactions_helper.saveAliases(reactions_aliases_dict)
 
 
-def refine_model(gpfilledModel, draftModel=None):
+def refine_model(gpfilledModel, draftModel=None, scale = 1000, unscalled = None):
+    '''
+    refine a model from the gapfiller
+
+    Parameters
+    ----------
+    gpfilledModel : cobra.model
+        a model that was returned by the gapfiller
+    draftModel : cobra.model, optional
+        an orginal draft model containing gene annotatins. The default is None.
+    scale : float, optional
+        value to scale the upper and lower bound, that were set to 1 in the gapfiller. The default is 1000.
+    unscalled : reaction list or dict, optional
+        reactions that should not be scalled. For instance the media reactions. The default is None.
+
+    Returns
+    -------
+    mc : cobra.model
+        a model that is ready to use
+
+    '''
     
     mc = gpfilledModel.copy()
     
-    for metab in gpfilledModel.metabolites:
+    for metab in mc.metabolites:
         
         
         
@@ -67,7 +87,7 @@ def refine_model(gpfilledModel, draftModel=None):
             metab.formula = ''
             metab.name = metab.id
     
-    for reac in gpfilledModel.reactions:
+    for reac in mc.reactions:
         reacName = reac.id.split('_')[0]
         #reacSufix = reac.id.split('_')[1]
         
@@ -84,8 +104,8 @@ def refine_model(gpfilledModel, draftModel=None):
         else:#not in modelSEED
             reac.annotation = {}
             reac.name = reac.id
-        
-  
+    
+    #Add genes from a draft model
     if draftModel is not None:
         for reaction in draftModel.reactions:
             if len(reaction.genes)>0:
@@ -95,9 +115,9 @@ def refine_model(gpfilledModel, draftModel=None):
     #change internal fluxes to +/- 1000            
     for reaction in mc.reactions:
         
-        if not reaction.boundary:
-            reaction.lower_bound *=1000.
-            reaction.upper_bound *= 1000.
+        if reaction.id not in unscalled:
+            reaction.lower_bound *= scale
+            reaction.upper_bound *= scale
             
         
             
--- scripts/reaction_class.py
+++ scripts/reaction_class.py
@@ -8,14 +8,10 @@ Created on Mon Jul 12 23:16:07 2021
 
 #imports
 import cobra
-
 import numpy as np
-
 import os
-
 import ast
 
-#import gurobipy as gu
 
 class Reaction:
     
@@ -101,11 +97,9 @@ class Reaction:
         reactions={}
         for reaction in model.reactions:
             
-            if 'EX_' not in reaction.id:
-            
-                reactions[reaction.id] = {'lower_bound': (reaction.lower_bound and abs(reaction.lower_bound)/reaction.lower_bound or 0), 'upper_bound': (reaction.upper_bound and abs(reaction.upper_bound)/reaction.upper_bound or 0)}
-                mets = reaction.metabolites
-                reactions[reaction.id]['metabolites']={i.id:mets[i] for i in mets}
+            reactions[reaction.id] = {'lower_bound': (reaction.lower_bound and abs(reaction.lower_bound)/reaction.lower_bound or 0), 'upper_bound': (reaction.upper_bound and abs(reaction.upper_bound)/reaction.upper_bound or 0)}
+            mets = reaction.metabolites
+            reactions[reaction.id]['metabolites']={i.id:mets[i] for i in mets}
                 
         return reactions
     
